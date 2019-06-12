24d134fb9f6e4a9b1565b5d9d554cf8ce86e2a81
raikausami <monnha2ndg@gmail.com>
2019-06-12 16:23:05 +0900

    1st commit for Portfolio 1st day

---
+++ Index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="/a/default.css" type="text/css">
+    </head>
+    <body>
+        <ul class="example">
+        <li>
+          <a href="#home">HOME</a>
+        </li>
+        <li>
+          <a href="#profile">PROFILE</a>
+        </li>
+        <li>
+          <a href="#portfolio">PORTFOLIO</a>
+        </li>
+        <li>
+          <a href="#exp">EXPERIENCE</a>
+        </li>
+        <li>
+          <a href="#contact">CONTACT</a>
+        </li>
+        </ul>
+
+        <p style="padding-top: 0px">
+        <a name="home"></a>
+        </p>
+
+        <p style="padding-top: 50px"></p>
+
+        <div class="profile" style="position: relative;">
+            <td><img src="g.jpg" width="1320" height="900" style="opacity: 0.3;"></td>
+            <div style="position: absolute; top: 50px; left: 1000px; width: 335px;">
+                <font size="7" color="#ffffff">01 PROFILE</font>
+            </div>
+            <div style="position: absolute; top: 150px; left: 50px; width: 200px;">
+            國澤　龍之介<br>
+            Kunisawa Ryunosuke<br>
+            1995年10月27日生まれ<br>
+            芝浦工業大学大学院<br>
+            理工学研究科<br>
+            国際理工学専攻<br>
+            </div>
+            <div style="position: absolute; top: 230px; left: 730px; width: 200px;">
+            <nobr><font size="6" color="#ffffff"> ITによる課題解決を日々学んでいます</font></nobr>
+            </div>
+        </div>
+
+        <div class="portfolio" style="position: relative;">
+            <div style="position: absolute; top: 100px; left: 1000px; width: 335px;">
+               <font size="7" color="#ffffff"><a name="portfolio">02 PORTFOLIO</a></font>
+            </div>
+        </div>
+
+        <p style="padding-top: 150px"></p>
+        <table border="0" cellspacing="0" cellpadding="1">
+        <tr>
+        <td>
+        <div class ="click">
+            <td><img src="e.jpg" width="430" height="350" style="opacity: 0.3;"></td>
+        </div>
+        <td><img src="f.jpg" width="430" height="350" style="opacity: 0.3;"></td>
+        <td><img src="c.jpg" width="430" height="350" style="opacity: 0.3;"></td>
+        </tr>
+        </table>
+        <ul class="my_port">
+        <p>拡張性のある社員の移動把握システムのプロトタイプ</p>
+        <p>心電波形と加速度を組み合わせた複合生体認証</p>
+        <p>オートバイ衝突防止システムのプロトタイプ</p>
+        </ul>
+        <div class="portfolio" style="position: relative;">
+            <div style="position: absolute; top: 150px; left: 970px; width: 335px;">
+               <font size="7" color="#ffffff"><a name="portfolio"><nobr>03 EXPERIENCE</nobr></a></font>
+            </div>
+        </div>
+        <p style="padding-top: 250px"></p>
+        <p>2017 Sept. - 2018 Feb.</p>
+        <p>オートバイ衝突防止システムのプロトタイプ実装</p>
+        <p>マレーシア留学中、現地の研究室にて Arduinoと距離センサを用いた衝突判定の実装を行う</p>
+        <p style="padding-top: 50px"></p>
+
+        <p>2018 March</p>
+        <p>部屋の入退室者管理システムのアプリケーション作成</p>
+        <p>ベトナム留学中、現地の生徒と協力し Arduinoとモーションセンサを用いた入退室管理アプリケーションの実装を行う</p>
+
+        <p style="padding-top: 50px"></p>
+        <p>2018 March - </p>
+        <p>心電波形と加速度を用いた生体認証の実装</p>
+        <p>大学のネットワーク系研究室で企業と共同研究を行い機械学習を用いて生体認証を実装し学会発表、現在改良中</p>
+
+        <p style="padding-top: 50px"></p>
+        <p>2018 Sept. - 2019 March</p>
+        <p>社員位置管理システムのプロトタイプ実装</p>
+        <p>企業の社内における現状把握、課題抽出、IoTによる解決法の提示、予算見積もり、実装まで行う</p>
+
+        <p style="padding-top: 50px"></p>
+        <p>2019 April - </p>
+        <p>深層学習を用いたシングルエンド音声品質評価の提案中</p>
+        <p>通話などの音声劣化要因の推定を行う手法を提案中</p>
+
+        <p style="padding-top: 50px"></p>
+        <p>Future</p>
+        <p>2019 Nov. - 2020 Jan.</p>
+        <p>ビッグデータについて研究留学予定</p>
+
+        <p style="padding-top: 100px">
+          <a name="contact">03 CONTACT</a>
+        </p>
+        <p>bp15029◯shibaura-it.ac.jp</p>
+        <p>◯を＠に</p>
+    </body>
+</html>
+
+
+
